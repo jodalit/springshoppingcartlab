ba436496d04225da631e98a2786a5d77c16a6f23
jodongongo <jodongongo@OSIPC195.gcosi.local>
2016-12-15 10:30:15 -0500

    Update and cleaning

--- shoppingonline/src/main/java/com/alithya/shoppingcart/model/Item.java
+++ shoppingonline/src/main/java/com/alithya/shoppingcart/model/Item.java
@@ -5,9 +5,6 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 public class Item implements Serializable {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private Long itemId;
 	private String itemName;
